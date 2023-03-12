4f11b7dd7019e2fd90b19d343e53207cc3763f7a
jiang_feng <fjiang44@usc.edu>
2023-03-11 16:58:06 -0800

    update

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SK94bcTgn', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3p7m2hpddu58ra9nsst1t3pomk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_UNI20i9IT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7rvsi0v9psao1aast64flekqrv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
